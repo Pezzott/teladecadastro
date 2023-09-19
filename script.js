f7112088a3450d5dc6351b86e153cd445267421f
Ferreira <AFERR136@ford.com>
2023-09-18 22:26:09 -0300

    foi criado pagina alimentos saudaveis e foi acressentado produtos na pagina principal

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ menuItems.addEventListener('click', (event) => {
 
 
 // Aqui termina o script para deixar com interatividade o menu hambúrguer da página principal
+
+
+// Aqui inicia o script para deixar com interatividade na pagina Alimentos Saudáveis
+
+// ordena os alimentos do menos preço para o maior
+
+document.getElementById('sortOption').addEventListener('change', function () {
+    const selectedOption = this.value;
+    const products = document.querySelectorAll('.product-images figure'); // Selecionando cada produto dentro de .product-images
+
+    const sortedProducts = Array.from(products).sort((a, b) => {
+        if (selectedOption === 'productName') {
+            const nameA = a.querySelector('h4').textContent; // Selecionando o nome do produto pelo elemento h4
+            const nameB = b.querySelector('h4').textContent;
+            return nameA.localeCompare(nameB);
+        } else if (selectedOption === 'price') {
+            const priceA = parseFloat(a.querySelector('p[style*="color: rgb(236,103,14)"]').textContent.replace('R$', '').trim()); // Selecionando o preço pelo estilo específico
+            const priceB = parseFloat(b.querySelector('p[style*="color: rgb(236,103,14)"]').textContent.replace('R$', '').trim());
+            return priceA - priceB;
+        }
+    });
+
+    const container = document.querySelector('.product-images'); // Selecionando o container dos produtos
+    sortedProducts.forEach(product => container.appendChild(product));
+});
